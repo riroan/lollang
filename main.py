bb51825748b0796bfc9d46874266042339bd5c11
riroan <riroan@naver.com>
2022-03-12 12:17:22 +0900

    목적 파이썬 파일 경로 추가

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", required=True, help="컴파일할 파일을 선택해주세요.")
+    parser.add_argument("--out", default="out.py", help="목적 파이썬 파일경로를 선택해주세요")
     args = parser.parse_args()
     cmp = Compiler()
-    cmp.compileFile(args.file)
+    cmp.compileFile(args.file, args.out)
