ba211a8de6bc88bebfdbd8bd3f5402a1aee7bd14
riroan <riroan@naver.com>
2022-03-13 13:17:10 +0900

    비교연산자 추가

--- operators.py
+++ operators.py
@@ -3,6 +3,7 @@ class Operator:
     GE = "ㅋ"
     LT = "ㄷ"
     LE = "ㅌ"
+    EQ = "ㅁ"
     ONE = "ㅠ"
     ADD = "ㅜ"
     SUB = "ㅡ"
@@ -10,8 +11,8 @@ class Operator:
     DIV = "ㅏ"
     INT_DIV = "ㅕ"
     REM = "ㅑ"
-    op = [">",">=","<","<=","+","-","*","/","//","%"]
+    op = [">",">=","<","<=", "==","+","-","*","/","//","%"]
         
     @staticmethod
     def getOp():
-        return [Operator.GT, Operator.GE, Operator.LT, Operator.LE, Operator.ADD, Operator.SUB,Operator.MUL,Operator.DIV,Operator.INT_DIV,Operator.REM]
+        return [Operator.GT, Operator.GE, Operator.LT, Operator.LE, Operator.EQ, Operator.ADD, Operator.SUB,Operator.MUL,Operator.DIV,Operator.INT_DIV,Operator.REM]
